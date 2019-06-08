c89a74054c73c348339b46ce8ddbbfca888fcdbd
Gurrehmat Oberoi <gurrehmat_oberoi@external.mckinsey.com>
2019-06-09 01:20:46 +0530

    Added planet/vehicle selection logic

--- src/components/Form/Form.component.js
+++ src/components/Form/Form.component.js
@@ -9,12 +9,23 @@ class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      planets: [],
-      vehicles: [],
-      selection: {
-        planetNames: [],
-        vehicleNames: [],
-      },
+      planets: {},
+      vehicles: {},
+      selected: [
+        {
+          planet: '',
+          vehicle: '',
+        }, {
+          planet: '',
+          vehicle: '',
+        }, {
+          planet: '',
+          vehicle: '',
+        }, {
+          planet: '',
+          vehicle: '',
+        },
+      ],
     };
     this.fetchPlanets();
     this.fetchVehicles();
@@ -26,36 +37,35 @@ class Form extends Component {
   fetchVehicles = () => Axios.get(GET_VEHICLES)
     .then(vehicles => this.setState({ vehicles: vehicles.data }));
 
-  makeSelection = (planet, vehicle) => {
-    const { selection } = this.state;
-    if (selection.planetNames.indexOf(planet) === -1) {
-      this.setState({
-        selection: {
-          planetNames: [
-            ...selection.planetNames,
-            planet,
-          ],
-          vehicleNames: {
-            ...selection.vehicleNames,
-            vehicle,
-          },
-        },
-      });
-    }
-  };
+  choosePlanet = (planetName, index) => {
+    const { selected } = this.state;
+    selected[index].planet = planetName;
+    this.setState({ selected });
+  }
+
+  chooseVehicle = (vehicleName, index) => {
+    const { selected } = this.state;
+    selected[index].vehicle = vehicleName;
+    this.setState({ selected });
+  }
 
   render() {
-    const { planets, vehicles, selection } = this.state;
+    const { planets, vehicles, selected } = this.state;
     return (
       <div className="formContainer">
         {(planets.length > 0) && (vehicles.length > 0) && (
-          <Selector
-            planets={planets}
-            vehicles={vehicles}
-            selection={selection}
-            makeSelection={this.makeSelection}
-          />
+          [0, 1, 2, 3].map(index => (
+            <Selector
+              planets={planets}
+              vehicles={vehicles}
+              selected={selected}
+              index={index}
+              choosePlanet={this.choosePlanet}
+              chooseVehicle={this.chooseVehicle}
+            />
+          ))
         )}
+
       </div>
     );
   }
--- src/components/Form/Selector/Selector.component.js
+++ src/components/Form/Selector/Selector.component.js
@@ -1,64 +1,129 @@
 import React, { Component } from 'react';
-import Select from 'react-select';
-import { Button } from 'react-bootstrap';
-import { DESTINATION, CONFIRM } from '../../../constants/strings';
+import Select, { components } from 'react-select';
+// import { Button } from 'react-bootstrap';
+import propTypes from 'prop-types';
+import { DESTINATION } from '../../../constants/strings';
 import './Selector.component.scss';
+
+const SingleValue = (props) => {
+  const { data } = props;
+  return (
+    <components.SingleValue {...props}>
+      {data.value}
+    </components.SingleValue>
+  );
+};
 
 class Selector extends Component {
   state = {
-    selectedPlanet: '',
-    selectedVehicle: '',
+    planetChosen: '',
   };
+
+  isPlanetDisabled = (planet) => {
+    const { selected } = this.props;
+    const selectedPlanets = selected.map(selection => selection.planet);
+    return (selectedPlanets.indexOf(planet.name) !== -1);
+  }
+
+  isVehicleDisabled = (vehicle) => {
+    const { planets } = this.props;
+    const { planetChosen } = this.state;
+    let planetDistance;
+    const planetSpecs = planets
+      .find(planet => ((planet.name === planetChosen) ? planet : undefined));
+    if (planetSpecs.distance !== undefined) {
+      planetDistance = planetSpecs.distance;
+    } else planetDistance = 0;
+    return ((this.getVehicleCount(vehicle.name) <= 0) || (vehicle.max_distance < planetDistance));
+  }
+
+  onPlanetChange = (planetChosen) => {
+    const { choosePlanet, index } = this.props;
+    this.setState({ planetChosen: planetChosen.value });
+    choosePlanet(planetChosen.value, index);
+  }
+
+  onVehicleChange = (vehicleChosen) => {
+    const { chooseVehicle, index } = this.props;
+    chooseVehicle(vehicleChosen.value, index);
+  }
+
+  vehicleSelectionUI = () => {
+    const { planetChosen } = this.state;
+    const { vehicles } = this.props;
+    if (planetChosen === '') {
+      return null;
+    }
+    const vehicleOptions = vehicles.map(vehicle => ({
+      value: vehicle.name,
+      label: `${vehicle.name} (${this.getVehicleCount(vehicle.name)})`,
+      isDisabled: this.isVehicleDisabled(vehicle),
+    }));
+    return (
+      <div className="dropdown">
+        <Select
+          options={vehicleOptions}
+          onChange={this.onVehicleChange}
+          components={{ SingleValue }}
+        />
+      </div>
+    );
+  }
+
+  getVehicleCount = (vehicleName) => {
+    const { vehicles, selected } = this.props;
+    const total = vehicles.find(vehicle => ((vehicle.name === vehicleName)
+      ? vehicle : undefined)).total_no;
+    const selectedNumber = selected.filter(selection => (selection.vehicle === vehicleName)).length;
+    return total - selectedNumber;
+  }
 
   render() {
     const {
-      planets, vehicles, selection, makeSelection,
+      planets, vehicles, index, selected,
     } = this.props;
-    const { selectedPlanet, selectedVehicle } = this.state;
-    const availablePlanets = planets.filter(
-      planet => selection.planetNames.indexOf(planet.name) === -1,
-    );
-    const availableVehicles = vehicles.map((vehicleType) => {
-      const count = vehicleType.total_no - selection.vehicleNames.reduce(
-        (total, name) => ((name === vehicleType.name) ? total + 1 : total), 0,
-      );
-      return {
-        value: vehicleType.name,
-        label: `${vehicleType.name} (${count})`,
-        isDisabled: (count <= 0) || ((selectedPlanet !== '') && vehicleType.max_distance < planets
-          .find(planet => ((planet.name === selectedPlanet) ? planet : undefined)).distance),
-      };
-    });
+    const planetOptions = planets.map(planet => ({
+      value: planet.name,
+      label: planet.name,
+      isDisabled: this.isPlanetDisabled(planet),
+    }));
     return (
       <section className="selector">
-        <span>{DESTINATION}</span>
+        <p>{`${DESTINATION} ${index + 1}`}</p>
         <div className="dropdown">
           <Select
-            options={availablePlanets.map(({ name }) => ({
-              value: name,
-              label: name,
-            }))}
-            onChange={option => this.setState({ selectedPlanet: option.value })}
+            options={planetOptions}
+            onChange={this.onPlanetChange}
           />
         </div>
-        {
-          (selectedPlanet !== '') && (
-            <div className="dropdown">
-              <Select
-                options={availableVehicles}
-                onChange={option => this.setState({ selectedVehicle: option.value })}
-              />
-            </div>
-          )
-        }
-        <Button
-          disabled={(selectedPlanet === '') || (selectedVehicle === '')}
-        >
-          {`${CONFIRM} ${DESTINATION}`}
-        </Button>
+        {this.vehicleSelectionUI()}
       </section>
     );
   }
 }
 
+Selector.propTypes = {
+  planets: propTypes.arrayOf(propTypes.shape({
+    name: propTypes.string,
+    distance: propTypes.number,
+  })).isRequired,
+  vehicles: propTypes.arrayOf(propTypes.shape({
+    name: propTypes.string,
+    total_no: propTypes.number,
+    max_distance: propTypes.number,
+    speed: propTypes.number,
+  })).isRequired,
+  selected: propTypes.arrayOf(propTypes.shape({
+    planet: propTypes.string,
+    vehicle: propTypes.string,
+  })).isRequired,
+  index: propTypes.number.isRequired,
+  choosePlanet: propTypes.func.isRequired,
+  chooseVehicle: propTypes.func.isRequired,
+};
+
+SingleValue.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  data: propTypes.object.isRequired,
+};
 export default Selector;
